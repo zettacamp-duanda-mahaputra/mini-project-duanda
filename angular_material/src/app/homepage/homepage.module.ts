52c3f07facf29787ba09070870964983db818cbc
Duanda Mahaputra <duandamahaputra1@gmail.com>
2022-11-29 16:18:49 +0700

    fix fcm

--- angular_material/src/app/homepage/homepage.module.ts
+++ angular_material/src/app/homepage/homepage.module.ts
@@ -8,7 +8,7 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { MenuComponent } from '../menu/menu.component';
 import { LoginComponent } from '../login/login.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { environment } from "../../environments/environment";
+import { environment } from "../../environments/environment.prod";
 import { initializeApp } from "firebase/app";
 
 initializeApp(environment.firebase);
--- angular_material/src/environments/environment.prod.ts
+++ angular_material/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCboTXQ9W23HXD2t-qM8PE1GnAMKhDKKAY",
+    authDomain: "psychic-apex-334206.firebaseapp.com",
+    databaseURL: "https://psychic-apex-334206-default-rtdb.firebaseio.com/",
+    projectId: "psychic-apex-334206",
+    storageBucket: "psychic-apex-334206.appspot.com",
+    messagingSenderId: "738057478646",
+    appId: "1:738057478646:web:1887444f60df21bb1a27d7",
+    measurementId: "G-YLPYL9Q4C2",
+    vapidKey: "BDsieRJlxCGTSYtK4DxVKeueeAKZxYPrOy8saMDEmdNUvEoOFk7CgG_ZUvOTeVw4E_8isbrtHvML0j0gBnI4j3Q"
+  }
 };
